8143a8311159c8ba0b3eb43e4246c66412bb693f
indafridge <x17491304@student.ncirl.ie>
2019-04-11 20:52:41 +0100

    Non working button clicks

--- app/src/main/java/com/example/gfit/HomePage.java
+++ app/src/main/java/com/example/gfit/HomePage.java
@@ -5,15 +5,17 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class HomePage extends AppCompatActivity {
-    ImageButton Camera;
-
+    private ImageButton Camera;
+    private Button Stats;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Stats = (Button)findViewById(R.id.btnStats);
     }
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -28,5 +30,12 @@ public class HomePage extends AppCompatActivity {
         // Do something in response to button click
         dispatchTakePictureIntent();
     }
+    Stats.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(HomePage.this, StatsPage.class)  ;
+            startActivity(intent);
+        }
+    });
 
 }
