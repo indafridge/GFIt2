c654d4eecdd3d0b0fe36527152b15aa8efa69c89
indafridge <x17491304@student.ncirl.ie>
2019-04-11 19:23:17 +0100

    Camera icon and take picture

--- app/src/main/java/com/example/gfit/HomePage.java
+++ app/src/main/java/com/example/gfit/HomePage.java
@@ -1,13 +1,32 @@
 package com.example.gfit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomePage extends AppCompatActivity {
+    ImageButton Camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
     }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    /** Called when the user touches the button */
+    public void sendMessage(View view) {
+        // Do something in response to button click
+        dispatchTakePictureIntent();
+    }
+
 }
