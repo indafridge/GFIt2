b759aee5612ee8cacd92b39ccb19c5a4580ce8e8
Mindaugas <mynde101@gmail.com>
2019-04-11 19:08:00 +0100

    Database connected to firebase

--- app/src/main/java/com/example/gfit/User.java
+++ app/src/main/java/com/example/gfit/User.java
@@ -4,14 +4,16 @@ public class User {
 
     public String username;
     public String password;
+    public String repassword;
     public String email;
 
     public User(){
 
     }
-    public User(String username,String password, String email){
+    public User(String username,String password, String repassword, String email){
         this.username = username;
         this.password = password;
+        this.repassword = repassword;
         this.email = email;
     }
 }
