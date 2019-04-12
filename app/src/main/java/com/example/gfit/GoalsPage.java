2458b1698ba97d28fa8814234ac719015efc7f4e
Byrnzee <byrneciaran11@gmail.com>
2019-04-12 18:48:15 +0100

    Goals page

--- app/src/main/java/com/example/gfit/GoalsPage.java
+++ app/src/main/java/com/example/gfit/GoalsPage.java
@@ -1,34 +1,17 @@
-/*import android.content.Intent;
+package com.example.gfit;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
-import com.example.gfit.R;
-import com.example.gfit.RegisterPage;
 
-port android.content.Intent;
-
-imort android.content.Intent;
-
-|*?package com.example.gfit;
-
-public class Goals extends AppCompatActivity {
+public class GoalsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goals);
+        setContentView(R.layout.activity_goals_page);
     }
 
-
-    Goals.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(this, RegisterPage.class)  ;
-            startActivity(intent);
-
-        }
-    }
 
 }
-*/
+
