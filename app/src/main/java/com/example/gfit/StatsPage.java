e24a822e4ef894fedc7c19c4dbec771e044f4751
Mindaugas <mynde101@gmail.com>
2019-04-12 17:23:14 +0100

    Stat page java

---
+++ app/src/main/java/com/example/gfit/StatsPage.java
@@ -0,0 +1,12 @@
+package com.example.gfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
