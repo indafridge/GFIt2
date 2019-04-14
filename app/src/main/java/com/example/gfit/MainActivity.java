251e886378eda1f0f780dec929615f22ad274f1e
Mindaugas <mynde101@gmail.com>
2019-04-14 14:22:18 +0100

    Bringing things back

--- app/src/main/java/com/example/gfit/MainActivity.java
+++ app/src/main/java/com/example/gfit/MainActivity.java
@@ -1,70 +1,88 @@
 package com.example.gfit;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-private EditText mEmail, mPass;
-private FirebaseAuth mAuth;
+
+    private EditText Email;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private Button Create;
+    private int counter =3;
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEmail=(EditText)findViewById(R.id.etEmail);
-        mPass=(EditText)findViewById(R.id.etPassword);
-        mAuth = FirebaseAuth.getInstance();
 
+        Email = (EditText)findViewById(R.id.etEmail);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        Login = (Button)findViewById(R.id.btnLogin);
+        Create = (Button)findViewById(R.id.btnCreate);
+
+        Info.setText("No of attempts remaining: 3");
+
+        Create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Intent intent = new Intent(MainActivity.this, RegisterPage.class)  ;
+                startActivity(intent);
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Email.getText().toString(), Password.getText().toString());
+            }
+        });
     }
-    public void clickSign(View view){
-        Intent intent = new Intent(MainActivity.this, RegisterPage.class);
-        startActivity(intent);
-    }
-    private void signIn(String email, String password) {
+    public void validate(String userName, String userPassword){
+        if((userName.equals("admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(this, HomePage.class);
+            startActivity(intent);
+        }
+        else{
+            counter--;
 
-        // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
+            Info.setText("No of attempts remaining: " +String.valueOf(counter));
 
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            if(user.isEmailVerified()){
-                                Toast.makeText(MainActivity.this,"Ok Login Success",Toast.LENGTH_SHORT);
-                            }
-
-                        } else {
-
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-
-                        // [START_EXCLUDE]
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-                        // [END_EXCLUDE]
-                    }
-                });
-        // [END sign_in_with_email]
-    }
-
-    public void Login(View view) {
-        signIn(mEmail.getText().toString(),mPass.getText().toString());
+            if(counter == 0){
+                Login.setEnabled(false);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/gfit/MainActivity2.java
@@ -0,0 +1,71 @@
+/*package com.example.gfit;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+private EditText mEmail, mPass;
+private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        mEmail=(EditText)findViewById(R.id.etEmail);
+        mPass=(EditText)findViewById(R.id.etPassword);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+    public void clickSign(View view){
+        Intent intent = new Intent(MainActivity.this, RegisterPage.class);
+        startActivity(intent);
+    }
+    private void signIn(String email, String password) {
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if(user.isEmailVerified()){
+                                Toast.makeText(MainActivity.this,"Ok Login Success",Toast.LENGTH_SHORT);
+                            }
+
+                        } else {
+
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    public void Login(View view) {
+        signIn(mEmail.getText().toString(),mPass.getText().toString());
+    }
+}
+*/
